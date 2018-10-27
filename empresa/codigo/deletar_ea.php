b54a5e1f3bfeecb34eb5b989f4cd6098887bcad8
sigabriel <36973489+sigabriel@users.noreply.github.com>
2018-10-26 22:15:17 -0300

    Update deletar_ea.php

--- empresa/codigo/deletar_ea.php
+++ empresa/codigo/deletar_ea.php
@@ -1,7 +1,7 @@
 <?php
 	$id = $_POST['id'];
 	
-	$connect = mysqli_connect('localhost','root','','test');
+	$connect = mysqli_connect('127.0.0.1:50018','azure','6#vWHD_$','test');
 	
 	$sql = "DELETE FROM empresa_atelier WHERE id = '".$id."'";
 	$result = mysqli_query($connect,$sql);
