167815109994ddf8e1c484d1d2a761e4076db296
sigabriel <36973489+sigabriel@users.noreply.github.com>
2018-11-01 21:22:17 -0300

    Update login_atelier.php

--- atelier/codigo/login_atelier.php
+++ atelier/codigo/login_atelier.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
 
-$connect = mysqli_connect('localhost','root','','test');
+$connect = mysqli_connect('127.0.0.1:50018','azure','6#vWHD_$','test');
 
  
   
